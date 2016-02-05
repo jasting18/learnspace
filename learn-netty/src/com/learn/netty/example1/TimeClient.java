472ca7cc9d0493cbe1aa87ca432b0202eb48332d
yingshen <yingshen@163.com>
2016-02-05 18:12:41 +0800

    client

--- learn-netty/src/com/learn/netty/example1/TimeClient.java
+++ learn-netty/src/com/learn/netty/example1/TimeClient.java
@@ -6,49 +6,17 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.CountDownLatch;
 
 public class TimeClient {
 	public static void main(String[] args){
-		int port  = 8080;
-		String serverIp = "10.128.221.54";
+		int num = 1000;
+		CountDownLatch semaphore = new CountDownLatch(1);
 		
-		Socket socket = null;
-		BufferedReader in = null;
-		PrintWriter out = null;
-		
-		try {
-			socket = new Socket(serverIp,port);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new PrintWriter(socket.getOutputStream(),true);
-			out.println("query time");
-			System.out.println("send order to server success.");
-			String resp = in.readLine();
-			System.out.println("Time is :"+resp);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally{
-			if (in!=null){
-				try {
-					in.close();
-					in = null;
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-				if (out!=null){
-					out.close();
-					out = null;
-				}
-				if (socket!=null){
-					try {
-						socket.close();
-						socket = null;
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
+		for (int i=0;i<num;i++){
+			new Thread(new TimeQuery(semaphore)).start();
+			System.out.println(i);
 		}
+		semaphore.countDown();
 	}
 }
---
+++ learn-netty/src/com/learn/netty/example1/TimeQuery.java
@@ -0,0 +1,65 @@
+package com.learn.netty.example1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.CountDownLatch;
+
+public class TimeQuery implements Runnable {
+	private CountDownLatch semaphore ;
+	public TimeQuery(CountDownLatch semaphore){
+		this.semaphore = semaphore;
+	}
+	@Override
+	public void run() {
+		int port  = 8080;
+		String serverIp = "10.128.221.54";
+		
+		Socket socket = null;
+		BufferedReader in = null;
+		PrintWriter out = null;
+		
+		try {
+			this.semaphore.await();
+			
+			socket = new Socket(serverIp,port);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(),true);
+			out.println("query time");
+			System.out.println("send order to server success.");
+			String resp = in.readLine();
+			System.out.println("Time is :"+resp);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}finally{
+			if (in!=null){
+				try {
+					in.close();
+					in = null;
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				if (out!=null){
+					out.close();
+					out = null;
+				}
+				if (socket!=null){
+					try {
+						socket.close();
+						socket = null;
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+
+}
