5f002eb74bba19bd6a66388acbf9111075a8a184
yingshen <yingshen@163.com>
2016-02-13 09:24:09 +0800

    example2

---
+++ learn-netty/src/com/learn/netty/example2/TimeClient.java
@@ -0,0 +1,19 @@
+package com.learn.netty.example2;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TimeClient {
+	public static void main(String[] args){
+		int num = 1000;
+//		CountDownLatch semaphore = new CountDownLatch(1);
+//		
+//		for (int i=0;i<num;i++){
+//			new Thread(new TimeQuery(semaphore)).start();
+//			System.out.println(i);
+//		}
+//		semaphore.countDown();
+		
+		TimeClientHandle clientHandle = new TimeClientHandle("127.0.0.1",8080);
+		new Thread(clientHandle).start();
+	}
+}
